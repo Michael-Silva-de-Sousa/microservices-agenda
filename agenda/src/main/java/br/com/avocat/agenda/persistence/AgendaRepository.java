e15374f8114238553b2300c578611abaf8683e45
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-14 22:43:30 -0300

    V0.15 - Ajuste na classe de repository e ajuste na conexão com o mongodb.

--- agenda/src/main/java/br/com/avocat/agenda/persistence/AgendaRepository.java
+++ agenda/src/main/java/br/com/avocat/agenda/persistence/AgendaRepository.java
@@ -1,14 +1,10 @@
 package br.com.avocat.agenda.persistence;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
 
 @Repository
 public interface AgendaRepository extends MongoRepository<Agenda, String> {
 
-    Page<Agenda> findAll(LocalDateTime dataInicial, LocalDateTime dataFinal, Pageable pageable);
+    //Page<Agenda> findAll(LocalDateTime dataInicial, LocalDateTime dataFinal, Pageable pageable);
 }
