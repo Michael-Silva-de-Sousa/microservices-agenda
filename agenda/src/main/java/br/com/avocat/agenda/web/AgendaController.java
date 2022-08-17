a3f6ff5a50bcaef3976e647327112117b193013e
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-17 09:37:57 -0300

    V0.31 - Retirada da interface AgendaController. - Ajuste na Controller de Agendas.

--- agenda/src/main/java/br/com/avocat/agenda/web/AgendaController.java
+++ agenda/src/main/java/br/com/avocat/agenda/web/AgendaController.java
@@ -1,34 +1,72 @@
 package br.com.avocat.agenda.web;
 
+import br.com.avocat.agenda.exception.AgendaException;
 import br.com.avocat.agenda.persistence.Agenda;
+import br.com.avocat.agenda.service.AgendaServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 
-public interface AgendaController {
+@AllArgsConstructor
+@RequestMapping("/v1/api/agenda")
+@RestController
+public class AgendaController {
+
+    private AgendaServiceImpl agendaService;
 
     @PostMapping
-    ResponseEntity<Agenda> salvar(@RequestBody @Valid Agenda agenda);
+    public ResponseEntity<Agenda> salvar(@RequestBody @Valid Agenda agenda) {
+
+        try {
+            var result = agendaService.salvar(agenda);
+            return result
+                    .map(value -> ResponseEntity.ok().body(value))
+                    .orElseGet(() -> ResponseEntity.badRequest().build());
+
+        } catch (AgendaException e) {
+            throw new AgendaException("Erro ao salvar o agendamento:", e);
+        }
+    }
 
     @GetMapping("/pesquisar")
-    ResponseEntity<Page<Agenda>> pesquisarPorPeriodo(
-            @RequestParam(value = "dt-ini", required = true) String dataInicio,
+    public ResponseEntity<Page<Agenda>> pesquisarPorPeriodo(
+            @RequestParam(value = "dt-ini", required = true) String dataInicial,
             @RequestParam(value = "dt-fin", required = true) String dataFinal,
             @RequestParam(value="page", defaultValue = "0", required = false) Integer page,
-            @RequestParam(value="size", defaultValue = "10", required = false) Integer size);
+            @RequestParam(value="size", defaultValue = "10", required = false) Integer size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(agendaService.pesquisarPorPeriodo(dataInicial, dataFinal, pageable));
+    }
 
     @GetMapping("/{agendaId}")
-    ResponseEntity<Agenda> pesquisarPorId(@PathVariable("agendaId") String id);
+    public ResponseEntity<Agenda> pesquisarPorId(@PathVariable("agendaId") String agendaId) {
+        try {
+            return agendaService.pesquisarPorId(agendaId)
+                    .map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        } catch (AgendaException e) {
+            throw new AgendaException("Erro ao pesquisar o agendamento de ID: " + agendaId, e);
+        }
+    }
 
     @GetMapping("/processo/{processoId}")
-    ResponseEntity<Page<Agenda>> pesquisarPorProcessoId(@PathVariable("processoId") Long processoId);
+    public ResponseEntity<Page<Agenda>> pesquisarPorProcessoId(@PathVariable("processoId") Long processoId) {
+        return null;
+    }
 
     @GetMapping("/contrato/{contratoId}")
-    ResponseEntity<Page<Agenda>> pesquisarPorContratoId(@PathVariable("contratoId") Long contratoId);
+    public ResponseEntity<Page<Agenda>> pesquisarPorContratoId(@PathVariable("contratoId") Long contratoId) {
+        return null;
+    }
 
-    @DeleteMapping("/excluir")
-    ResponseEntity<Void> excluir(@PathVariable("/{id}") String id);
+    @DeleteMapping("/excluir/{agendaId}")
+    public ResponseEntity<Void> excluir(@PathVariable("agendaId") String id) {
+        agendaService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
 }
