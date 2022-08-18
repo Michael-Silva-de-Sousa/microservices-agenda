f1bf5e65df50467ea9e965f9810228c5b346fc54
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-18 08:24:15 -0300

    V0.43 - Criação e configuração do modulo de mensageria rabbitMQ.

---
+++ amqp/src/test/java/br/com/avocat/amqp/AmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avocat.amqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
