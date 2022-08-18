fb00e1fc3e89333a20cec2f0403dce213ac34e19
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-18 08:12:12 -0300

    V0.41 - Habilitado o eureka-client para os modulos Agenda e Notificacao.

--- agenda/src/main/java/br/com/avocat/agenda/AgendaApplication.java
+++ agenda/src/main/java/br/com/avocat/agenda/AgendaApplication.java
@@ -2,8 +2,10 @@ package br.com.avocat.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableEurekaClient
 @EnableMongoAuditing
 @SpringBootApplication
 public class AgendaApplication {
--- notificacao/src/main/java/br/com/avocat/notificacao/NotificacaoApplication.java
+++ notificacao/src/main/java/br/com/avocat/notificacao/NotificacaoApplication.java
@@ -2,7 +2,9 @@ package br.com.avocat.notificacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class NotificacaoApplication {
 
