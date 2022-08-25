62b33b1e8adf94cf0e10fb3792b8ac5f17f264d2
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-24 21:34:00 -0300

    V0.63 - Ajustado os Respositorys dos projetos Agenda e Notificacao para funcionar com o JPA.

--- agenda/src/main/java/br/com/avocat/agenda/persistence/AgendaRepository.java
+++ agenda/src/main/java/br/com/avocat/agenda/persistence/AgendaRepository.java
@@ -2,15 +2,15 @@ package br.com.avocat.agenda.persistence;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
 @Repository
-public interface AgendaRepository extends MongoRepository<Agenda, String> {
+public interface AgendaRepository extends JpaRepository<Agenda, String> {
 
-    @Query("{ 'dataCadastro':{ $gte: ?0, $lt: ?1} } ")
+    @Query("select a from  Agenda a where a.dataInicial >= ?1 and a.dataFinal =< ?2")
     Page<Agenda> pesquisaPorPeriodo(LocalDate dataInicial, LocalDate dataFinal, Pageable pageable);
 }
--- notificacao/src/main/java/br/com/avocat/notificacao/persistence/NotificacaoRepository.java
+++ notificacao/src/main/java/br/com/avocat/notificacao/persistence/NotificacaoRepository.java
@@ -1,8 +1,8 @@
 package br.com.avocat.notificacao.persistence;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificacaoRepository extends MongoRepository<Notificacao, String> {
+public interface NotificacaoRepository extends JpaRepository<Notificacao, String> {
 }
