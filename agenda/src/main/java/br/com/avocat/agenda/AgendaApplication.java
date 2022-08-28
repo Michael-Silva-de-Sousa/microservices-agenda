cdb9bd6fbf45d8d24d2cc35d8538590115732140
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-27 22:42:54 -0300

    V0.68 - Ajuste no Dockerfile. - Retirada de imports desnecessarios ao projeto.

--- agenda/src/main/java/br/com/avocat/agenda/AgendaApplication.java
+++ agenda/src/main/java/br/com/avocat/agenda/AgendaApplication.java
@@ -7,10 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableEurekaClient
-@EnableMongoAuditing
 @SpringBootApplication
 public class AgendaApplication {
 
