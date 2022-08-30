2ea4ffe49b6101b194ea9c735df4292fa838db6b
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-29 21:46:36 -0300

    V0.73 - Criação do repository.

---
+++ agenda/src/main/java/br/com/avocat/agenda/persistence/ProcessoRepository.java
@@ -0,0 +1,8 @@
+package br.com.avocat.agenda.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProcessoRepository extends JpaRepository<Processo, Long> {
+}
